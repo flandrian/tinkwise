68a1146a2fefb51f51699899e34f74a224202797
Florian Doerfler <vlaad@bluewin.ch>
2016-01-10 16:04:53 +0100

    auto format arduino code

--- arduino/wirelessreceiver/wirelessreceiver.ino
+++ arduino/wirelessreceiver/wirelessreceiver.ino
@@ -16,18 +16,18 @@ char float_buffer[FLOAT_BUFFER_SIZE];
 
 void setup()
 {
-    Serial.begin(115200);	//Transmit readings over this port
-    // Initialise the IO and ISR
-    vw_set_rx_pin(RXPIN);
-    vw_set_ptt_inverted(true); // Required for DR3100
-    vw_setup(2000);	 // Bits per sec
-    vw_rx_start();       // Start the receiver PLL running
+  Serial.begin(115200);	//Transmit readings over this port
+  // Initialise the IO and ISR
+  vw_set_rx_pin(RXPIN);
+  vw_set_ptt_inverted(true); // Required for DR3100
+  vw_setup(2000);	 // Bits per sec
+  vw_rx_start();       // Start the receiver PLL running
 }
 
 void ftos(char *buf, float value)
 {
-    int16_t value_int = value;
-    sprintf(buf, "%d", value_int);
+  int16_t value_int = value;
+  sprintf(buf, "%d", value_int);
 }
 
 void get_test_message(uint8_t *buf, uint8_t *buf_len)
@@ -36,39 +36,39 @@ void get_test_message(uint8_t *buf, uint8_t *buf_len)
   *(buf++) = 23; // node ID
   *(buf++) = 1;  // temperature measurement code
   *(float*)buf = 42.0f;  // dummy value
-  *buf_len = 1+1+4;
+  *buf_len = 1 + 1 + 4;
 }
 
 void loop()
 {
-    uint8_t buf[VW_MAX_MESSAGE_LEN];
-    uint8_t buflen = VW_MAX_MESSAGE_LEN;
+  uint8_t buf[VW_MAX_MESSAGE_LEN];
+  uint8_t buflen = VW_MAX_MESSAGE_LEN;
 
-    digitalWrite(13, true); // Flash a light to show received good message
+  digitalWrite(13, true); // Flash a light to show received good message
 
-    get_test_message(buf, &buflen);
-//    if (vw_get_message(buf, &buflen)) // Non-blocking
+  get_test_message(buf, &buflen);
+  //    if (vw_get_message(buf, &buflen)) // Non-blocking
+  {
+    int buf_index = 0;
+    //        Serial.print("{\"node\":");
+    uint8_t node_index = *(uint8_t*)(buf + buf_index);
+    buf_index += 1;
+    Serial.print(node_index);
+    //        Serial.print(",");
+    Serial.print(":");
+    while (buf_index < buflen)
     {
-        int buf_index = 0;
-//        Serial.print("{\"node\":");
-        uint8_t node_index = *(uint8_t*)(buf+buf_index);
-        buf_index += 1;
-        Serial.print(node_index);
-//        Serial.print(",");
-        Serial.print(":");
-        while(buf_index < buflen)
-        {
-            uint8_t data_type = *(uint8_t*)(buf+buf_index);
-            buf_index++;
-            float data = *(float*)(buf+buf_index);
-            buf_index += 4;
-//            Serial.print("\"temperature\":");
-            ftos(float_buffer, data);
-            Serial.print(float_buffer);
-//            Serial.print(",");
-        }
-//            Serial.println("}");
-            Serial.println();
-            digitalWrite(13, false);
+      uint8_t data_type = *(uint8_t*)(buf + buf_index);
+      buf_index++;
+      float data = *(float*)(buf + buf_index);
+      buf_index += 4;
+      //            Serial.print("\"temperature\":");
+      ftos(float_buffer, data);
+      Serial.print(float_buffer);
+      //            Serial.print(",");
     }
+    //            Serial.println("}");
+    Serial.println();
+    digitalWrite(13, false);
+  }
 }
--- arduino/wirelesstransmitter/wirelesstransmitter.ino
+++ arduino/wirelesstransmitter/wirelesstransmitter.ino
@@ -17,65 +17,65 @@ int sensorPin = 6;        // the analog pin the LM35's Vout (sense) pin is conne
 int sensorSupplyPin = 10;  // the digital pin that the LM35's supply is connected to
 
 int wakeupIndex = 0;
-int wakeupCount = 70; // one deep sleep phase is about 8s                                    
+int wakeupCount = 70; // one deep sleep phase is about 8s
 
 // watchdog interrupt//
-ISR (WDT_vect) 
+ISR (WDT_vect)
 {
   wdt_disable();                   // disable watchdog//
-}  
+}
 
 void setup() {
-  Serial.begin(115200); 
+  Serial.begin(115200);
   randomSeed(analogRead(A0));  //initialize the random number generator with
                                //a random read from an unused and floating analog port
   //initialize the virtual wire library
   vw_set_ptt_inverted(true); // Required for DR3100
   vw_set_tx_pin(TRANSPIN);
   vw_setup(2000);  //keep the data rate low for better reliability and range
-  
+
   pinMode(sensorSupplyPin, OUTPUT);      // sets the digital pin as output
 }
 
 int ftoa(char *a, float f)  //translates floating point readings into strings to send over the air
 {
-  int left=int(f);
-  float decimal = f-left;
-  int right = decimal *100; //2 decimal points
+  int left = int(f);
+  float decimal = f - left;
+  int right = decimal * 100; //2 decimal points
   if (right > 10) {  //if the decimal has two places already. Otherwise
-    sprintf(a, "%d.%d",left,right);
-  } else { 
-    sprintf(a, "%d.0%d",left,right); //pad with a leading 0
+    sprintf(a, "%d.%d", left, right);
+  } else {
+    sprintf(a, "%d.0%d", left, right); //pad with a leading 0
   }
 }
 
-int xmitMessage(char *msg, int msg_len){
-    digitalWrite(13, true); // Flash an led to show transmitting
-    vw_send((uint8_t *)msg, msg_len);
-    vw_wait_tx(); // Wait until the whole message is gone
-    digitalWrite(13, false);
+int xmitMessage(char *msg, int msg_len) {
+  digitalWrite(13, true); // Flash an led to show transmitting
+  vw_send((uint8_t *)msg, msg_len);
+  vw_wait_tx(); // Wait until the whole message is gone
+  digitalWrite(13, false);
 }
 
 void measureAndSend()
 {
-    char message[50];
+  char message[50];
 
-    digitalWrite(sensorSupplyPin, HIGH);    // enable supply for lm35
-    delay(1); //let sensor input settle
-    int reading = analogRead(sensorPin);    // getting the voltage reading from the temperature sensor
-    digitalWrite(sensorSupplyPin, LOW);    // disable supply for lm35
-    // converting that reading to voltage, for 3.3v arduino use 3.3
-    float voltage = reading * 5.0;
-    voltage /= 1024.0;
-    // now print out the temperature
-    float temperature = voltage * 100 ; //converting from 10 mv per degree
+  digitalWrite(sensorSupplyPin, HIGH);    // enable supply for lm35
+  delay(1); //let sensor input settle
+  int reading = analogRead(sensorPin);    // getting the voltage reading from the temperature sensor
+  digitalWrite(sensorSupplyPin, LOW);    // disable supply for lm35
+  // converting that reading to voltage, for 3.3v arduino use 3.3
+  float voltage = reading * 5.0;
+  voltage /= 1024.0;
+  // now print out the temperature
+  float temperature = voltage * 100 ; //converting from 10 mv per degree
 
- //build the message
+  //build the message
   char temp_string[6]; //2 int, 2 dec, 1 point, and \0
-  ftoa(temp_string,temperature);
+  ftoa(temp_string, temperature);
 
-    sprintf(message, "%d:temperature:%s", MYID, temp_string);  //millis provides a stamp for deduping if signal is repeated
-    Serial.println(message);
+  sprintf(message, "%d:temperature:%s", MYID, temp_string);  //millis provides a stamp for deduping if signal is repeated
+  Serial.println(message);
 
   void *buffer_ptr = message;
   int message_size = 0;
@@ -92,13 +92,13 @@ void get_test_message(uint8_t *buf, uint8_t *buf_len)
   *(buf++) = 23; // node ID
   *(buf++) = 1;  // temperature measurement code
   *(float*)buf = 42.0f;  // dummy value
-  *buf_len = 1+1+4;
+  *buf_len = 1 + 1 + 4;
 }
 
 void send_encoded_message()
 {
   uint8_t message[50];
-  
+
   void *buffer_ptr = message;
   uint8_t message_size = 0;
   *(uint8_t*)buffer_ptr = MYID;
@@ -122,7 +122,7 @@ int append_data(void** buffer, uint8_t meaning_code, T data)
 void send_test_message()
 {
   uint8_t message[50];
-  
+
   void *buffer_ptr = message;
   uint8_t message_size = 0;
   get_test_message((uint8_t*)buffer_ptr, &message_size);
@@ -136,18 +136,18 @@ void loop() {
   {
     measureAndSend();
     wakeupIndex = wakeupCount;
-//    unsigned long randNumber = random(60,120); //1 to 2 minutes to delay
+    //    unsigned long randNumber = random(60,120); //1 to 2 minutes to delay
   }
   wakeupIndex--;
 
- 
+
   byte old_ADCSRA = ADCSRA;                        // disable ADC //
   ADCSRA = 0;                                      // disable ADC //
 
   byte old_PRR = PRR;                              // disable Internal modules//
   PRR = 0xFF;                                      // disable Internal modules//
 
-  MCUSR = 0;                                       // clear various "reset" flags// 
+  MCUSR = 0;                                       // clear various "reset" flags//
 
   // Watchdog Timer Parameters//
   WDTCSR = bit (WDCE) | bit (WDE);                 // allow changes, disable reset
