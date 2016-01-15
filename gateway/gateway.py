dd056d3359c2d0f20eed91c9d7cf2eaa82fc8bc8
Florian Doerfler <vlaad@bluewin.ch>
2016-01-15 22:25:13 +0100

    make gateway executable

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -1,32 +1,35 @@
+#!/usr/bin/python
+
 from serial import Serial
 from time import sleep
 import json
 import rrdtool
 
-CARBON_SERVER = '0.0.0.0'
-CARBON_PORT = 2003
-
-#serial_file = file("test_data.txt")
-serial_file = Serial(port="/dev/ttyUSB0", baudrate=115200)
-
-while True:
-	line = serial_file.readline()
-	if line == '':
-		sleep(1)
-		continue
-	print 'processing ' + line
-	try:
-		sample = json.loads(line)
-	except Exception, e:
-		print 'could not parse JSON string ' + line
-		print e
-		continue
-	if not 'node' in sample:
-		print 'no node found in JSON string' + line
-		continue
-	node_index = int(sample['node'])
-	print 'node ' + str(node_index)
-	for item in sample.items():
-		if item[0] == 'temperature':
-			print 'temp: ' + str(item[1])
-			rrdtool.update('rrd/{}.rrd'.format(node_index), 'N:{}'.format(item[1]))
+def main():
+	serial_file = file("test_data.txt")
+	#serial_file = Serial(port="/dev/ttyUSB0", baudrate=115200)
+	
+	while True:
+		line = serial_file.readline()
+		if line == '':
+			sleep(1)
+			continue
+		print 'processing ' + line
+		try:
+			sample = json.loads(line)
+		except Exception, e:
+			print 'could not parse JSON string ' + line
+			print e
+			continue
+		if not 'node' in sample:
+			print 'no node found in JSON string' + line
+			continue
+		node_index = int(sample['node'])
+		print 'node ' + str(node_index)
+		for item in sample.items():
+			if item[0] == 'temperature':
+				print 'temp: ' + str(item[1])
+				rrdtool.update('rrd/{}.rrd'.format(node_index), 'N:{}'.format(item[1]))
+				
+if __name__ == "__main__":
+	main()
