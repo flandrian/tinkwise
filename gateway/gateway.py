c5e442a122b305f3a0e47669bca1e213fc827fa4
Florian Doerfler <vlaad@bluewin.ch>
2016-01-29 22:52:16 +0100

    log to syslog

--- gateway/gateway.py
+++ gateway/gateway.py
@@ -7,9 +7,14 @@ import rrdtool
 from ConfigParser import ConfigParser
 import os
 import argparse
+import logging.handlers
 
 class TinkwiseGateway(object):
 	def __init__(self):
+		self._logger = logging.getLogger()
+		self._logger.setLevel(logging.INFO)
+		self._logger.addHandler(logging.handlers.SysLogHandler(address='/dev/log'))
+
 		parser = argparse.ArgumentParser(description='tinkwise gateway')
 		parser.add_argument('-c','--config_file', type=str, dest='config_file', default='/etc/tinkwise.conf')
 		args = parser.parse_args()
@@ -23,7 +28,11 @@ class TinkwiseGateway(object):
 		if connection_type == 'file':
 			self._input_file = file(connection_file)
 		elif connection_type == 'serial':
-			self._input_file = Serial(port=connection_file, baudrate=115200)
+			try:
+				self._input_file = Serial(port=connection_file, baudrate=115200)
+			except Exception as e:
+				self._logger.critical('failed to open serial port ' + connection_file + ':' + e.message)
+				raise
 			
 	def run(self):
 		while True:
@@ -31,18 +40,18 @@ class TinkwiseGateway(object):
 			if line == '':
 				sleep(1)
 				continue
-			print 'processing ' + line
+			self._logger.info('processing ' + line)
 			try:
 				sample = json.loads(line)
 			except Exception, e:
-				print 'could not parse JSON string ' + line
-				print e
+				self._logger.error('could not parse JSON string ' + line)
+				self._logger.error(e.message)
 				continue
 			if not 'node' in sample:
-				print 'no node found in JSON string' + line
+				self._logger.error('no node found in JSON string' + line)
 				continue
 			node_index = int(sample['node'])
-			print 'node ' + str(node_index)
+			self._logger.info('node ' + str(node_index))
 			
 			del sample['node']
 			
@@ -57,14 +66,18 @@ class TinkwiseGateway(object):
 				rrdtool.update(rrd_path, '--template', data_sources, 'N:' + ':'.join(data_values))
 			except Exception as e:
 				if e.message.find('unknown DS name') >= 0:
-					print 'data source reveived is missing in config file'
+					self._logger.warning('data source reveived is missing in config file')
 				else:
 					raise
 					
 	def _create_database(self, path, data_sources):
-		print 'creating database {} with sources {}'.format(path, ','.join(data_sources))
+		self._logger.info('creating database {} with sources {}'.format(path, ','.join(data_sources)))
 		data_source_specs = map(lambda s: 'DS:'+s+':GAUGE:600:U:U', data_sources)
-		rrdtool.create(path, '--step', '600', data_source_specs, 'RRA:LAST:0.5:1:80000')
+		try:
+			rrdtool.create(path, '--step', '600', data_source_specs, 'RRA:LAST:0.5:1:80000')
+		except Exception as e:
+			self._logger.error(e.message)
+			raise
 
 if __name__ == "__main__":
 	gateway = TinkwiseGateway()
